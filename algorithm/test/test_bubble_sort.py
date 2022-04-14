340375d97d6d38b24dd83e65f710f984d3be5bf4
umepon0626 <ryuichiastrona@gmail.com>
2022-04-14 13:47:35 +0900

    bubble_sortのテスト追加

--- algorithm/test/test_bubble_sort.py
+++ algorithm/test/test_bubble_sort.py
@@ -7,3 +7,7 @@ def test_bubble_sort1():
 def test_bubble_sort2():
   test_arr = [1,3,4,6,2,5]
   assert bubble_sort.bubble_sort(test_arr) == [1,2,3,4,5,6]
+
+def test_bubble_sort3():
+  test_arr = [2,1]
+  assert bubble_sort.bubble_sort(test_arr) == [1,2]
